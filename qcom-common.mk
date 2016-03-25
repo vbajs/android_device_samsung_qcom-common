7296cfcd0ec6939aaedaabbf94c601ab0f5a461d
Yahya Wessam <yahyawessam2002@gmail.com>
2016-03-25 15:32:58 +0300

    Update qcom-common.mk

--- qcom-common.mk
+++ qcom-common.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The Phoenix Project
+# Copyright (C) 2016 The Turbo Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
